a50b7d942e4439122b84460c0aa2d682aedc59ea
Rizqi Nur Assyaufi <bandithijo@gmail.com>
2022-08-07 11:44:47 +0800

    Change THUMB_SIZE to 4

--- config.def.h
+++ config.def.h
@@ -68,7 +68,7 @@ static const int CACHE_SIZE_FALLBACK = 32 * 1024 * 1024; /* fallback to 32MiB if
 static const int thumb_sizes[] = { 32, 64, 96, 128, 160 };
 
 /* thumbnail size at startup, index into thumb_sizes[]: */
-static const int THUMB_SIZE = 3;
+static const int THUMB_SIZE = 4;
 
 #endif
 #ifdef _MAPPINGS_CONFIG
